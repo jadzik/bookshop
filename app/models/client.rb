3bfe3e30dc5c54c8ca83abcdf7bae6da6460bb27
Jadzia Gruczka <jjadzikk@gmail.com>
2012-02-07 21:33:18 +0100

    client model fixed

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class Client < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  attr_accessible :email, :password, :password_confirmation, :remember_me#, :resource
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   :prerequisite_attributes
   has_many :addresses, :as => :resource
   has_many :payers
@@ -9,29 +9,24 @@ class Client < ActiveRecord::Base
 
   private
    def phone_cannot_be_blank
-      if mobile_phone.blank? and fax_phone.blank? and stationary_phone.blank?
+      if mobile_phone.blank? and stationary_phone.blank?
         errors.add(:mobile_phone,"Musisz podać przynajmniej jeden numer telefonu!")
       end
     end
-    def type_of_client
 
-company:
-  validates_presence_of  :nip
-  validates_uniqueness_of  :nip
-
-    end
-
-
-  validates_presence_of :name,
-  validates :email, :format => {:with =>  /^\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z$/, 
-            :message => "To nie jest e-mail"}
+  validates_presence_of :name, :email, :type
   validate :phone_cannot_be_blank
-  validates :nip, :format => { :with =>  /^[0-9]{10}$/, :message => "NIP jest złożony z 10 cyfr"}, :allow_blank => true
+  with_options :if => :type != "indywidualny" do |company|
+    company.validates :nip, :presence => true, :uniqueness =>true, 
+                            :format => { :with =>  /^[0-9]{10}$/, :message => "NIP jest złożony z 10 cyfr"}
+  end
+  validates :email, :format => {:with =>  /^\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z$/, 
+            :message => "To nie jest e-mail"}, :uniqueness => true
   validates :mobile_phone, :format => { :with =>  /^[0-9]{9}$/, 
             :message => "numer telefony komórkowego jest złożony z 9 cyfr"}, :allow_blank => true
   validates :fax_phone, :format => {:with =>  /^\([0-9]{2}\)[0-9]{7}$/, 
             :message => "numer musi składać się z nr kierunkowego w nawiasach i numeru właściwego"},:allow_blank => true
   validates :stationary_phone, :format => {:with =>  /^\([0-9]{2}\)[0-9]{7}$/, 
             :message => "numer musi składać się z nr kierunkowego w nawiasach i numeru właściwego"},:allow_blank => true
-
+  validates_inclusion_of :typ, :in => %w(indywidualny firma szkoła biblioteka)
 end
--- app/models/payer.rb
+++ app/models/payer.rb
@@ -5,15 +5,15 @@ class Payer < ActiveRecord::Base
 
   private
     def library_or_school_validation
-      unless  client.type(@company_id) == "Biblioteka" || client.type(@company_id) == "Szkoła"
+      unless  client.type(@client_id) == "Biblioteka" || client.type(@client_id) == "szkoła"
         errors.add( "Twoja firma nie jest szkołą ani biblioteką, więc nie możesz dodać płatników.")
       end
     end
 
-  validates_presence_of :name, :phone, :company_id, :nip
+  validates_presence_of :name, :phone, :client_id, :nip
   validate :library_or_school_validation
   validates :nip, :format => { :with =>  /^[0-9]{10}$/, :message => "NIP jest złożony z 10 cyfr"}
-  validates_numericality_of :company_id
+  validates_numericality_of :client_id
   validates :phone, :format => { :with =>  /^([0-9]{9}$)|^(\([0-9]{2}\)[0-9]{7})$/, 
             :message => "numer telefony komórkowego jest złożony z 9 cyfr, a stacjonarny składa się z nr kierunkowego w nawiasach i numeru właściwego"}
 end
