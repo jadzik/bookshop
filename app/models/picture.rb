1112bb7062d9952837203fdbb0aae2c21e81ee76
Jadzia Gruczka <jjadzikk@gmail.com>
2012-03-26 23:16:49 +0200

    gallery

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
   belongs_to :product
-  has_attached_file :photo, :styles => { :small  => "150x150>" },
+  has_attached_file :photo, :styles => { :small  => "150x150>" , :large => "500x500>"},
                     :url  => "/assets/products/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 validates_attachment_presence :photo
