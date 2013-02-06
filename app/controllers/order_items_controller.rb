abe9a45b1d67a134b93a38541cbac46be3dd9a64
Jadzia Gruczka <jjadzikk@gmail.com>
2013-02-06 22:33:35 +0100

    this step to cart fixed his error

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -8,12 +8,7 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     product = Product.find(params[:product_id])
-    if product.resource_type == "Book"
-      name = product.resource.title     
-    else
-      name = product.resource.name
-    end
-    @order_item = @order.add_product(product.id, product.resource.price, name)
+    @order_item = @order.add_product(product.id, product.resource.price, product.resource.name)
     @order_item.product = product
     if @order_item.save
       redirect_to @order_item.order, notice: "Dodałeś produkt do koszyka" 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,13 +3,13 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
-  attr_accessible :order_id, :product_id
+  attr_accessible :order_id, :product_id, :price_for_one, :name_of_product
 
   def total_price
-    product.resource.price * amount
+    price_for_one * amount
   end
 
-  validates_presence_of :product_id, :order_id, :amount,  :price_for_one#, :name_of_product
+  validates_presence_of :product_id, :order_id, :amount,  :price_for_one, :name_of_product
   validates_numericality_of :product_id, :order_id
   validates :price_for_one, :numericality => { :precision => 2 }
   validates :amount, :numericality => { :greater_than => 0, :less_than => 999, :only_integer => true} 
